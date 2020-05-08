3503854262d66b117c41aa6cea7360c54885fd2b
samiuljoy <samiuljoy46@gmail.com>
2020-05-09 01:08:50 +0600

    lets see

---
+++ toggle.js
@@ -0,0 +1,43 @@
+// check for saved 'darkMode' in localStorage
+let darkMode = localStorage.getItem('darkMode'); 
+const darkModeToggle = document.querySelector('#mode');
+var word = document.getElementById("mode");
+var meta = document.querySelector("meta[name=theme-color]");
+
+const enableDarkMode = () => {
+  // 1. Add the class to the body
+  document.body.classList.add('D');
+  meta.setAttribute("content", "#10101c");
+  word.innerHTML = "🐭";
+  // 2. Update darkMode in localStorage
+  localStorage.setItem('darkMode', 'enabled');
+}
+
+const disableDarkMode = () => {
+  // 1. Remove the class from the body
+  document.body.classList.remove('D');
+  meta.setAttribute("content", "#002f30");
+  word.innerHTML = "🐼";
+  // 2. Update darkMode in localStorage 
+  localStorage.setItem('darkMode', null);
+}
+ 
+// If the user already visited and enabled darkMode
+// start things off with it on
+if (darkMode === 'enabled') {
+  enableDarkMode();
+}
+
+// When someone clicks the button
+darkModeToggle.addEventListener('click', () => {
+  // get their darkMode setting
+  darkMode = localStorage.getItem('darkMode'); 
+  
+  // if it not current enabled, enable it
+  if (darkMode !== 'enabled') {
+    enableDarkMode();
+  // if it has been enabled, turn it off  
+  } else {  
+    disableDarkMode(); 
+  }
+});
